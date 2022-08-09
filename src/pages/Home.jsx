7abdece1789448685d2b084a02afe0e8a3c34dc0
fabriciotex <fabriciortex@gmail.com>
2022-08-09 01:12:46 -0700

    Create home

--- src/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <h1>Sign-in sheet</h1>;
-}
-
-export default Home;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export function Home() {
+  return (
+    <div className="container">
+      <h1>Sign-in sheet</h1>
+      <input type="text" placeholder="Type in your name..." />
+      <button type="button">Sign in</button>
+    </div>
+  );
+}
